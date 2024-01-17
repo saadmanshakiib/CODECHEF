f31ee5bcd813ae0598669c5fa802f6e4624553d0
Sadman Al Sakib <150785896+saadmanshakiib@users.noreply.github.com>
2024-01-17 21:06:08 +0600

    Add files via upload

---
+++ biriyaniclasses.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    if(t>=1&&t<=pow(10,4)){
+        while(t--){
+            int x,y;
+            cin>>x>>y;
+        cout<<x*y<<endl;
+    }
+    }
+}
