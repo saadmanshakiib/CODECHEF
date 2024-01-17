60aa2e246265d5ac87c88c1b7fced0fe2dbbb084
Sadman Al Sakib <150785896+saadmanshakiib@users.noreply.github.com>
2024-01-17 23:34:18 +0600

    Add files via upload

---
+++ donationdrive.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+            if(t>=1&&t<=200){
+        int x,n;
+        cin>>n>>x;
+        if(x,n>=1&&x,n<=20&&x<=n){
+            cout<<n-x<<endl;
+        }
+    }
+    }
+}
