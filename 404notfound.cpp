e28d681df700af8bf93b9a7f604f54b4ae9ea2b5
Sadman Al Sakib <150785896+saadmanshakiib@users.noreply.github.com>
2024-01-17 23:15:00 +0600

    Add files via upload

---
+++ 404notfound.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+        if(t==404){
+            cout<<"NOT FOUND"<<endl;
+        }
+        else{
+            cout<<"FOUND"<<endl;
+        }
+    
+}
