abaf3002c1c433c6484a8ddce3d27ca3142d8850
Sadman Al Sakib <150785896+saadmanshakiib@users.noreply.github.com>
2024-01-17 21:01:26 +0600

    Add files via upload

---
+++ mastercheffinals.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    if(t>=1&&t<=100){
+        while(t--){
+            int x;
+            cin>>x;
+            if(x>=10){
+        cout<<"Yes"<<endl;
+    }
+    else{
+        cout<<"No"<<endl;
+        }
+    }
+    }
+}
