4d86b8414fd524371949a379951a4a814f5d9864
Sadman Al Sakib <150785896+saadmanshakiib@users.noreply.github.com>
2024-01-17 23:36:47 +0600

    Add files via upload

---
+++ kitchentimings.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    if(t>=1&&t<=100){
+    while(t--){
+        int x,y;
+        cin>>x>>y;
+        if(x,y>=1&&x,y<=12&&x<y){
+            cout<<y-x<<endl;
+        }
+    }
+    }
+}
