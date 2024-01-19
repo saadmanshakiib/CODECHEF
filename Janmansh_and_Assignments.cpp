125e3a6d38dd28344b365e98e3a5b3d39db38ad3
Sadman Al Sakib <150785896+saadmanshakiib@users.noreply.github.com>
2024-01-19 23:34:34 +0600

    Add files via upload

---
+++ Janmansh_and_Assignments.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x,t;
+    cin>>t;
+    if(t>=1&&t<=10){
+        while(t--){
+            cin>>x;
+    if(x>=1&&x<=9){
+        if(10-x>=3)
+    cout<<"YES"<<endl;
+    else
+    cout<<"NO"<<endl;
+        }
+    }
+    }
+    
+}
